651a63698862da62b8f1647d6c3f9f1f6c9b562a
mrtklcmrt <95560545+mrtklcmrt@users.noreply.github.com>
2023-02-09 16:21:46 +0300

    main sinifi

---
+++ maasHesaplamaMain.java
@@ -0,0 +1,8 @@
+package salaaryCalculation;
+
+public class maasHesaplamaMain {
+    public static void main(String[] args) {
+        EmployeeSalaary calisan1 = new EmployeeSalaary("mert", 3999, 65, 2012);
+        calisan1.infoEmployee();
+    }
+}
